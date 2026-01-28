c7ad0fb456904c2715de4c7d17e82bbdf8999457
Dom056 <dominicdawson99@gmail.com>
2026-01-28 20:47:58 +0000

    Added images to buttons

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,14 +1,19 @@
+import GitHub from '../Assets/github.png'
+import LinkedIn from '../Assets/linkedin.png'
+import Email from '../Assets/email.png'
+import Phone from '../Assets/phone-call.png'
+import './Contact.css'
+
 function  Contact() {
     return (
         <div className="contact">
             <p>If you'd like to get in touch, feel free to reach out to me :)</p>
             <h3>Get in Touch:</h3>
-            <button>Email Me</button>
-            <button>Phone</button>
+            <button><img src={Email} alt="Email" /> Email Me</button>
+            <button><img src={Phone} alt="Phone" /> Phone</button>
             <h3>Or find me on:</h3>
-            <button>LinkedIn</button>
-            <button>GitHub</button>
-            <button>Teams</button>
+            <button><img src={LinkedIn} alt="LinkedIn" /> LinkedIn</button>
+            <button><img src={GitHub} alt="GitHub" /> GitHub</button>
         </div>
     )
 }
